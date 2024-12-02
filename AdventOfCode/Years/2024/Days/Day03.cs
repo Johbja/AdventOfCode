7e51a80d14f49113d865b3611015f8869b3a589c
Johannes Bjärehall <jojeskate@hotmail.com>
2024-12-02 16:06:29 +0100

    Solved day 1 and 2 for year 2024

---
+++ AdventOfCode/Years/2024/Days/Day03.cs
@@ -0,0 +1,26 @@
+﻿using AdventOfCode.Infrastructure.Attributes;
+using AdventOfCode.Infrastructure.Interfaces;
+using AdventOfCode.Infrastructure.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Years._2024.Days;
+
+[DayInfo("", "Day 3")]
+public class Day03(LoadInputService inputService) : ISolution
+{
+    private readonly string[] _input = inputService.GetInputAsLines(nameof(Day03), 2024);
+
+    public void SolvePartOne()
+    {
+
+    }
+
+    public void SolvePartTwo()
+    {
+
+    }
+}
