4431689acbd18a9d2a48f1d5d90c94ab9bcfe17d
Johannes Bjärehall <jojeskate@hotmail.com>
2024-12-04 09:56:55 +0100

    Solved day 4

---
+++ AdventOfCode/Years/2024/Days/Day05.cs
@@ -0,0 +1,21 @@
+﻿using AdventOfCode.Infrastructure.Attributes;
+using AdventOfCode.Infrastructure.Interfaces;
+using AdventOfCode.Infrastructure.Services;
+
+namespace AdventOfCode.Years._2024.Days;
+
+[DayInfo("", "Day 5")]
+public class Day05(LoadInputService inputService) : ISolution
+{
+    private readonly string[] _input = inputService.GetInputAsLines(nameof(Day05), 2024);
+
+    public void SolvePartOne()
+    {
+        
+    }
+
+    public void SolvePartTwo()
+    {
+        
+    }
+}
