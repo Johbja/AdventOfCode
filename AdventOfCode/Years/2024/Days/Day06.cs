f93487bb829ab49936531aad04dc4016369296bc
Johannes Bjärehall <jojeskate@hotmail.com>
2024-12-06 14:29:49 +0100

    day 6 part 1 cleanup

--- AdventOfCode/Years/2024/Days/Day06.cs
+++ AdventOfCode/Years/2024/Days/Day06.cs
@@ -14,7 +14,7 @@ public class Day06 : ISolution
 
     public Day06(LoadInputService inputService)
     {
-        _input  = inputService.GetInputAsLines(nameof(Day06), 2024);
+        _input = inputService.GetInputAsLines(nameof(Day06), 2024);
     }
 
     private int EvaluateCharacter(char value)
@@ -22,88 +22,44 @@ public class Day06 : ISolution
 
     public void SolvePartOne()
     {
-        var map = _input.Select(row => row.Select(col => EvaluateCharacter(col)).ToArray()).ToArray();
-        var guardPos = map.SelectMany((row, y) => row.Select((value, x) => (x, y, value)))
+        var grid = _input.Select(row => row.Select(col => EvaluateCharacter(col)).ToArray()).ToArray();
+        var guardPos = grid.SelectMany((row, y) => row.Select((value, x) => (x, y, value)))
                           .Where(x => x.value == 2)
                           .Select(pos => (pos.x, pos.y))
                           .Single();
 
-        map[guardPos.y][guardPos.x] = 0;
+        grid[guardPos.y][guardPos.x] = 0;
+
+        int[] yMoveset = [-1, 0, 1, 0];
+        int[] xMoveset = [0, 1, 0, -1];
 
         Direction currentDirection = Direction.Up;
-        Dictionary<(int x, int y), int> visitedPositions = new() 
-        { 
-            { guardPos , 1} 
+        Dictionary<(int x, int y), int> visitedPositions = new()
+        {
+            { guardPos , 1}
         };
 
-        while(guardPos.x > 0 && guardPos.x < map[0].Length && guardPos.y > 0 && guardPos.y < map.Length)
+        while (guardPos.x > 0 && guardPos.x < grid[0].Length && guardPos.y > 0 && guardPos.y < grid.Length)
         {
-            if(currentDirection == Direction.Up)
-            {
-                if (guardPos.y - 1 < 0)
-                {
-                    guardPos = (guardPos.x ,guardPos.y - 1);
-                    break;
-                }
+            var (x, y) = (guardPos.x + xMoveset[(int)currentDirection], guardPos.y + yMoveset[(int)currentDirection]);
 
-                if (map[guardPos.y - 1][guardPos.x] == 0)
-                    guardPos = (guardPos.x, guardPos.y - 1);
-                else
-                    currentDirection = (Direction)(((int)currentDirection + 1) % 4);
+            if (x >= 0 && x < grid[0].Length && y >= 0 && y < grid.Length && grid[y][x] == 1)
+            {
+                currentDirection = (Direction)(((int)currentDirection + 1) % 4);
+            }
+            else
+            {
+                guardPos = (x, y);
             }
 
-            if (currentDirection == Direction.Down)
+            if(x >= 0 && x < grid[0].Length && y >= 0 && y < grid.Length)
             {
-                if (guardPos.y + 1 >= map.Length)
-                {
-                    guardPos = (guardPos.x, guardPos.y + 1);
-                    break;
-                }
-
-                if (map[guardPos.y + 1][guardPos.x] == 0)
-                    guardPos = (guardPos.x, guardPos.y + 1);
-                else
-                    currentDirection = (Direction)(((int)currentDirection + 1) % 4);
+                visitedPositions.TryAdd(guardPos, 0);
+                visitedPositions[guardPos]++;
             }
-
-
-            if (currentDirection == Direction.Right)
-            {
-                if (guardPos.x + 1 >= map[0].Length)
-                {
-                    guardPos = (guardPos.x + 1, guardPos.y);
-                    break;
-                }
-
-                if (map[guardPos.y][guardPos.x + 1] == 0)
-                    guardPos = (guardPos.x + 1, guardPos.y);
-                else
-                    currentDirection = (Direction)(((int)currentDirection + 1) % 4);
-            }
-
-
-            if (currentDirection == Direction.Left)
-            {
-                if (guardPos.x - 1 >= map[0].Length)
-                {
-                    guardPos = (guardPos.x - 1, guardPos.y);
-                    break;
-                }
-
-                if (map[guardPos.y][guardPos.x - 1] == 0)
-                    guardPos = (guardPos.x - 1, guardPos.y);
-                else
-                    currentDirection = (Direction)(((int)currentDirection + 1) % 4);
-            }
-
-            if (!visitedPositions.ContainsKey(guardPos))
-                visitedPositions.Add(guardPos, 0);
-
-            visitedPositions[guardPos]++;
         }
 
-        int result = visitedPositions.Count;
-        Console.WriteLine(result);
+        Console.WriteLine(visitedPositions.Count);
     }
 
     public void SolvePartTwo()
